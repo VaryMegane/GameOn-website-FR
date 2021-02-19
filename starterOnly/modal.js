6bb16d0d49ba4e8c03bb7956a05bdabd50c40965
Megane <meganevary@live.fr>
2021-02-19 14:47:53 +0100

    fermeture du formulaire active

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,24 @@ function editNav() {
   }
 }
 
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClose = document.querySelectorAll(".close"); /*span (x) fermeture formulaire */
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtnClose.forEach((span) => span.addEventListener("click", Modalclose));
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
+// close modal form
+function Modalclose() {
+  modalbg.style.display = "none";
+}
 
